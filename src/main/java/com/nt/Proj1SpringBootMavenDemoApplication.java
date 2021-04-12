0de8f95a7154b94643f076d113a4f20fcfa47cb5
dell <dell@WINDOWS-EC6DPLD>
2021-04-12 15:07:30 +0530

    FirstApplicationTestGit1

---
+++ src/main/java/com/nt/Proj1SpringBootMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj1SpringBootMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj1SpringBootMavenDemoApplication.class, args);
+	}
+
+}
